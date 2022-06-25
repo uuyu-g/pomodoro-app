750d698c4c5a273a1ab49e036b2e150cd6f4678e
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2022-06-25 17:00:39 +0900

    init angular

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pomodoro-app-root',
+  template: ` <pomodoro-app-nx-welcome></pomodoro-app-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'main';
+}
